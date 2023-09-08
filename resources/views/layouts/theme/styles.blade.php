5f6e2bd0ef08515d317f22700bfd4cc21ca01d55
Nesgc <nestorgarciac11@gmail.com>
2023-09-08 13:05:33 -0600

    bugs

--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -1,19 +1,58 @@
-    <link rel="icon" type="image/x-icon" href="../src/assets/img/favicon.ico"/>
-    <link href="../layouts/vertical-light-menu/css/light/loader.css" rel="stylesheet" type="text/css" />
-    <link href="../layouts/vertical-light-menu/css/dark/loader.css" rel="stylesheet" type="text/css" />
-    <script src="../layouts/vertical-light-menu/loader.js"></script>
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="../src/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="../layouts/vertical-light-menu/css/light/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="../layouts/vertical-light-menu/css/dark/plugins.css" rel="stylesheet" type="text/css" />
-    <!-- END GLOBAL MANDATORY STYLES -->
+<style>
 
-    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
-    <link href="../src/plugins/src/apex/apexcharts.css" rel="stylesheet" type="text/css">
-    <link href="../src/assets/css/light/components/list-group.css" rel="stylesheet" type="text/css">
-    <link href="../src/assets/css/light/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+.modal-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-    <link href="../src/assets/css/dark/components/list-group.css" rel="stylesheet" type="text/css">
-    <link href="../src/assets/css/dark/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
-    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+.btn {
+    font-weight: bold;
+    color: white;
+    padding: 0.5rem 1rem;
+    border-radius: 0.375rem;
+    cursor: pointer;
+}
+
+.btn-red {
+    background-color: #DC2626;
+    transition: background-color 0.2s ease-in-out;
+}
+
+.btn-red:hover {
+    background-color: #EF4444;
+}
+
+.btn-blue {
+    background-color: #2563EB;
+    transition: background-color 0.2s ease-in-out;
+}
+
+.btn-blue:hover {
+    background-color: #3B82F6;
+}
+
+.btn-green {
+    background-color: #059669;
+    transition: background-color 0.2s ease-in-out;
+}
+
+.btn-green:hover {
+    background-color: #10B981;
+}
+
+.form-control {
+    border-color: #D1D5DB; /* border-gray-300 */
+    border-radius: 0.375rem; /* rounded-md */
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1); /* shadow-sm */
+    transition: all 0.15s ease-in-out; /* For focus transition */
+}
+
+.form-control:focus {
+    border-color: #6366F1; /* focus:border-indigo-500 */
+    box-shadow: 0 0 0 3px rgba(99, 102, 241, 0.5); /* focus:ring-indigo-500 */
+}
+
+</style>
